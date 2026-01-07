c72ecb24495dbdd5e355e7e6a16a3e75632b09c6
bulltarddarcy <bulltarddarcy@gmail.com>
2026-01-07 11:42:48 +0000

    math

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import yfinance as yf
+import math
 from datetime import date, timedelta
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
