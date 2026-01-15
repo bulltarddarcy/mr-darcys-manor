f818ad18b896a5f6cdd64137c14c361d304ceddb
bulltarddarcy <bulltarddarcy@gmail.com>
2026-01-15 11:09:53 +0000

    Update main_sector.py

--- main_sector.py
+++ main_sector.py
@@ -518,6 +518,7 @@ def run_theme_momentum_app(df_global=None):
         st.caption("Copy tickers:")
         st.code(", ".join(df_filtered['Ticker'].unique().tolist()), language="text")
 
+    # divider line between the outputs (above) and the admin inputs/configurations (below)
     st.markdown('<hr style="border-top: 4px solid black;">', unsafe_allow_html=True)
     
     # ==========================================
